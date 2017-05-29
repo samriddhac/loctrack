1a03ac5c9ed1ae58a1dc68de18589dafc9d0c979
Samriddha Chatterjee <Samriddha.Chatterjee@Pearson.com>
2017-05-29 18:28:43 +0530

    Server v1

--- loctrackerserver/src/server.js
+++ loctrackerserver/src/server.js
@@ -1,9 +1,12 @@
+'use strict';
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const pub = require('redis-connection')();
 const sub = require('redis-connection')('subscriber');
-const events = require('./EventConstants');
+const events = require('./event-constants');
+const socketpool = require('./websocket-pool');
+
 
 app.get('/', function(req, res){
   res.send({
@@ -11,20 +14,53 @@ app.get('/', function(req, res){
   });
 });
 
-let allconnections = [];
 let subscribedconnections = [];
 
-io.on('connection', function(socket){
-	console.log('connection');
-	allconnections.push(socket);
+io.on(events.CONNECTION, function(socket){
+	console.log('Received connection request');
+
 	socket.on(events.EVENT_CONNECTION_ESTABLISHED, (from, data)=>{
-		
-	});  
-	socket.on(events.EVENT_ESTABLISH_CHANNEL, (from, data)=>{
-		
+		socketpool.addToPool({id:from, websocket:socket});
+		socket.emit(events.EVENT_ON_MESSAGE_RECEIVE, {id:from,t:events.TYPE_CONN_ACK});
 	});
+
+	socket.on(events.EVENT_ESTABLISH_AUTH, (from, data)=>{
+		let obj = {
+			id: from,
+			sub:[],
+			pub:[],
+			auth: {
+				isAuth: false,
+				otp: "1235"
+			}
+		};
+		pub.set(from, JSON.stringify(obj);
+		let websocket = socketpool.getConnectionByID(from);
+		websocket.emit(events.EVENT_ON_MESSAGE_RECEIVE, {...obj, t:events.TYPE_AUTH_VALIDATE});
+	});
+
+	socket.on(events.EVENT_ESTABLISH_AUTH_SUCCESS, (from, data)=>{
+		let objString = pub.get(from);
+		if(objString!==undefined && objString!==null) {
+			let obj = JSON.parse(objString);
+			obj  = {...obj, auth:{isAuth: true}};
+			let websocket = socketpool.getConnectionByID(from);
+			websocket.emit(events.EVENT_ON_MESSAGE_RECEIVE, {...obj, t:events.TYPE_ACK});
+		}
+	});
+
+	socket.on(events.EVENT_ESTABLISH_AUTH_FAILURE, (from, data)=>{
+		let objString = pub.get(from);
+		if(objString!==undefined && objString!==null) {
+			let obj = JSON.parse(objString);
+			let websocket = socketpool.getConnectionByID(from);
+			websocket.emit(events.EVENT_ON_MESSAGE_RECEIVE, {...obj, t:events.TYPE_ACK, auth:{isAuth: false}});
+		}
+	});
+
 	socket.on(events.EVENT_REQUEST_SUBSCRIPTION, (from, data)=>{
-		let dataObj = JSON.parse(data);
+		let toObj = JSON.parse(data);
+		
 		console.log(dataObj, from);
 		subscribedconnections.push({
 			channel:dataObj.to,
@@ -33,12 +69,6 @@ io.on('connection', function(socket){
 		sub.subscribe(dataObj.to);
 	});
 	socket.on(events.EVENT_STOP_SUBSCRIPTION, (from, data)=>{
-		
-	});
-	socket.on(events.EVENT_ALLOW_SUBSCRIPTION, (from, data)=>{
-		
-	});
-	socket.on(events.EVENT_DENY_SUBSCRIPTION, (from, data)=>{
 		
 	});
 	socket.on(events.EVENT_PUBLISH_LOCATION, (from, data)=>{
---
+++ loctrackerserver/src/websocket-pool.js
@@ -0,0 +1,65 @@
+
+let webSocketPool = [];
+
+function websocket_pool() {
+	this.webSocketPool =[];
+
+	return {
+
+		addToPool: function(conn) {
+			if(this.webSocketPool!==undefined && this.webSocketPool!==null) {
+				if(this.webSocketPool.length === 0 ){
+					this.webSocketPool.push(conn);
+				}
+				else {
+					let exists = false;
+					this.webSocketPool.forEach((item)=>{
+						if(item.id ==conn.id) {
+							exists = true;
+						}
+					});
+					if(!exists) {
+						this.webSocketPool.push(conn);
+					}
+				}
+			}
+		},
+		removeFromPool: function(conn) {
+			if(this.webSocketPool!==undefined && this.webSocketPool!==null) {
+				if(this.webSocketPool.length !== 0 ){
+					let matchIndex = -1;
+					for(let index=0; index<this.webSocketPool.length; index++) {
+						let item = this.webSocketPool[index];
+						if(item.id ==conn.id) {
+							matchIndex = index;
+						}
+					}
+					if(matchIndex>0) {
+						this.webSocketPool.splice(matchIndex, 1);
+					}
+				}
+			}
+		},
+		getConnectionByID: function(id) {
+			if(this.webSocketPool!==undefined && this.webSocketPool!==null
+				&& this.webSocketPool.length !== 0) {
+				let selectedConnection = {};
+				this.webSocketPool.forEach((item)=>{
+					if(item.id ==conn.id) {
+						selectedConnection = item;
+					}
+				});
+			}
+			return selectedConnection;
+		},
+		getAllConnection: function() {
+			return this.webSocketPool;
+		},
+		removeAllConnection() {
+			this.webSocketPool =[];
+		}
+
+	}
+}
+
+module.exports = websocket_pool;

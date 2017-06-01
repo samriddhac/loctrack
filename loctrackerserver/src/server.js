f4dbfa955753251a1d5eec77446d214edd6a63a2
Samriddha Chatterjee <Samriddha.Chatterjee@Pearson.com>
2017-06-01 18:38:38 +0530

    Server fix v1

--- loctrackerserver/src/server.js
+++ loctrackerserver/src/server.js
@@ -30,7 +30,8 @@ io.on(events.CONNECTION, function(socket){
 			console.log('Connection received from ', from, 'socket id ', socket.id);
 			let websocket = socketpool.getConnectionByID(from);
 			let wSocketId = websocket.websocket;
-			socket.broadcast.to(socket.id).emit(events.EVENT_ON_MESSAGE_RECEIVE, from, {id:from,t:events.TYPE_CONN_ACK});
+			socket.emit(events.EVENT_ON_MESSAGE_RECEIVE, from, {id:from,t:events.TYPE_CONN_ACK});
+			//socket.broadcast.to(socket.id).emit(events.EVENT_ON_MESSAGE_RECEIVE, from, {id:from,t:events.TYPE_CONN_ACK});
 		}
 		catch(err) {
 			console.log(err);
